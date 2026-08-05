8fa4f1e157ee36e9ca674f5eb0dc8640abb97c57
madinasagatova <2021255@student.cct.ie>
2026-08-05 21:22:36 +0100

    Add first job card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css'
 
+type JobApplication = {
+  company: String;
+  role: String;
+  location: String;
+  status: String;
+};
 function App() {
+  const job: JobApplication = {
+    company: "MongDB",
+    role: "Graduate Software Engineer",
+    location: "Dubin",
+    status: "Interview",
+  };
+
   return (
     <main>
       <h1>ApplyFlow</h1>
       <p>My Job Application Tracker</p>
+      <section>
+        <h2>{job.company}</h2>
+        <p>{job.role}</p>
+        <p>Location: {job.location}</p>
+        <p>Status: {job.status}</p>
+      </section>
     </main>
   )
 }
