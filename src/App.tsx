d6d39dff8e8a3e14c839cb1fee7e7c3ec7d5e4fd
madinasagatova <2021255@student.cct.ie>
2026-08-07 00:16:04 +0100

    Add new application modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // the modal is initially closed
   const [isModalOpen, setIsModalOpen] = useState(false);
 
 
@@ -21,17 +22,45 @@ function App() {
 
       {isModalOpen && (
         <div className='modal-overlay'>
-          <div className='modal' role='dialog' aria-modal="true" aria-labelledby='modal-title'>
+          <section
+          className='modal' 
+          role='dialog' 
+          aria-modal="true" 
+          aria-labelledby='modal-title'>
             <h2 id='modal-title'>Add a new application</h2>
-            <p>Your application form will go here.</p>
-            <button onClick={() => setIsModalOpen(false)}>
+            <div className='job-form'>
+              <label>Role<input type="text" placeholder='Software Engineer'/></label>
+              <label>Company<input type="text" placeholder='For example, Google'/>
+              </label>
+
+            <div className='form-row'>
+              <label>Applied date <input type="date" />
+              </label>
+              <label>Status
+                <select defaultValue={"Applied"}>
+                  <option value="Saved">Saved</option>
+                  <option value="Applied">Applied</option>
+                  <option value="Assessment">Assessment</option>
+                  <option value="Interview">Interview</option>
+                  <option value="Offer">Offer</option>
+                  <option value="Rejected">Rejected</option>
+                </select>
+              </label>
+            </div>
+            <div className='modal-actions'>
+            <button type='button' className='cancel-button' onClick={() => setIsModalOpen(false)}>
               Cancel
+            </button>
+            <button type='button' className='add-button'>
+              Add Application
             </button>
           </div>
         </div>
+      </section>
+      </div>
       )}
     </main>
   );
 }
 
-export default App
+export default App;
