152ff4b9cb50d7bfc65f82f6558ad0a374fcf5b8
madinasagatova <2021255@student.cct.ie>
2026-08-07 14:36:38 +0100

    Add applications from modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
 import { useState } from 'react';
 import './App.css';
 
+// Describe information that every job must contain
+type JobStatus =
+| "Saved"
+| "Applied"
+| "Assessment"
+| "Interview"
+| "Offer"
+| "Rejected";
+
+type JobApplication = {
+  id: number;
+  role: string;
+  company: string;
+  appliedDate: string;
+  status: JobStatus;
+};
+
+
 function App() {
-  // the modal is initially closed
+  
+
+  // The modal is initially closed
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  // React remembers an array of jobs applications
+  const [jobs, setJobs] = useState<JobApplication[]>([]);
 
+  // "" - means it starts empty
+  const [company, setCompany] = useState(""); 
+  const [role, setRole] = useState("");
+  const [appliedDate, setAppliedDate] = useState("");
+  const [status, setStatus] = useState<JobStatus>("Applied");
+
+  // This function runs ONLY when we click Add Application
+  function handleAddApplication() {
+    if (!role.trim() || !company.trim()) {
+      alert("Please enter both role and company.");
+      return;
+    }
+
+    const newJob: JobApplication = {
+      id: Date.now(),
+      role: role,
+      company: company,
+      appliedDate: appliedDate,
+      status: status,
+    };
+
+    // Adds a new job
+    setJobs((currentJobs) => [...currentJobs, newJob]);
+
+    setRole("");
+    setCompany("");
+    setAppliedDate("");
+    setStatus("Applied");
+    setIsModalOpen(false);
+
+  }
   return (
     <main>
       <header className='page-header'>
@@ -19,6 +72,29 @@ function App() {
           New Application
         </button>
       </header>
+      <section className='jobs-list'>
+        <h2>Your Applications</h2>
+         {/* Jobs array is empty */}
+        {jobs.length === 0 ? (
+          <p>No applications yet. Add your first one!</p>
+        ) : (
+          jobs.map((job) => (
+            <article className='job-card' key={job.id}>
+              <div>
+                <h3>{job.role}</h3>
+                <p>{job.company}</p>
+                <small>
+                  Applied: {job.appliedDate || "No date selected"}
+                </small>
+              </div>
+
+              <span className={`status-badge status-${job.status.toLowerCase()}`}>
+                {job.status}
+              </span>
+            </article>
+          ))
+        )}
+      </section>
 
       {isModalOpen && (
         <div className='modal-overlay'>
@@ -29,15 +105,15 @@ function App() {
           aria-labelledby='modal-title'>
             <h2 id='modal-title'>Add a new application</h2>
             <div className='job-form'>
-              <label>Role<input type="text" placeholder='Software Engineer'/></label>
-              <label>Company<input type="text" placeholder='For example, Google'/>
+              <label>Role<input type="text" placeholder='Software Engineer' value={role} onChange={(event) => setRole(event.target.value)}/></label>
+              <label>Company<input type="text" placeholder='For example, Google' value={company} onChange={(event) => setCompany(event.target.value)}/>
               </label>
 
             <div className='form-row'>
-              <label>Applied date <input type="date" />
+              <label>Applied date <input type="date" value={appliedDate} onChange={(event) => setAppliedDate(event.target.value)}/>
               </label>
               <label>Status
-                <select defaultValue={"Applied"}>
+                <select value={status} onChange={(event) => setStatus(event.target.value as JobStatus)}>
                   <option value="Saved">Saved</option>
                   <option value="Applied">Applied</option>
                   <option value="Assessment">Assessment</option>
@@ -51,13 +127,17 @@ function App() {
             <button type='button' className='cancel-button' onClick={() => setIsModalOpen(false)}>
               Cancel
             </button>
-            <button type='button' className='add-button'>
-              Add Application
-            </button>
-          </div>
+            <button
+                  type="button"
+                  className="add-button"
+                  onClick={handleAddApplication}
+                >
+                  Add Application
+                </button>
+              </div>
+            </div>
+          </section>
         </div>
-      </section>
-      </div>
       )}
     </main>
   );
